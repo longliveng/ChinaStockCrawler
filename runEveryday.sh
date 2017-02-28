c115a4a25d1bd08a1b58b60fe5ef9ed179f7a393
chan17 <cyh@fwhetou.top>
2017-02-28 11:45:16 +0800

    fixbug

--- runEveryday.sh
+++ runEveryday.sh
@@ -1,8 +1,9 @@
 #!/bin/bash                                                                                                                                           
-# 0 17 * * * sh /vagrant/code/ChinaStockCrawler/runEveryday.sh
+# 0 19 * * * cd /vagrant/code/ChinaStockCrawler/MainCrawler && /usr/local/bin/scrapy crawl stockExchangeTask
 # sed -i 's/\r//' script.sh
 
-cd /vagrant/code/ChinaStockCrawler/MainCrawler && scrapy crawl stockExchangeTask
+cd /vagrant/code/ChinaStockCrawler/MainCrawler
+/usr/local/bin/scrapy crawl stockExchangeTask
 
 # # PATH=$PATH:/usr/local/bin
 # # export PATH
